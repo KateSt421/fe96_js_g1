4c15c4dfa7f66f91a27309dce0752c63cdec4f44
Svetlana-Ruban86 <galasvetlana2020@gmail.com>
2025-04-19 11:03:28 +0300

    Add changes to intro.js

--- public/assets/intro.js
+++ public/assets/intro.js
@@ -1,3 +1,4 @@
+// Функция для кнопки "Выбрать" для перехода к каталогу
 const buttonCatalog=document.querySelector(`.text2_button_catalog`);
 buttonCatalog.addEventListener(`click`,function (){
     const catalogSection=document.getElementById(`plant-details`);
@@ -8,4 +9,9 @@ buttonCatalog.addEventListener(`click`,function (){
     }else{
         alert(`Раздел каталога не найден`);
     }
+      })
+
+// Функция для изображения рейтинга в яндекс для перехода на ссылку с адресом магазина
+      document.querySelector(`.yandex_link`).addEventListener(`click`,function(){
+        window.location.href="https://yandex.ru/maps/org/krapiva/228180338570/?ll=37.640482%2C55.740109&mode=search&sll=37.640482%2C55.740109&sspn=0.136860%2C0.058641&text=Магазины растений&z=13.4";
       })
